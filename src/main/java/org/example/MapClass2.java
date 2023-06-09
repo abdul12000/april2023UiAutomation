ba335020a21cd9b78d082cda68f8e51c1a164998
lateef abdulsalam <abdulsalam12000@hotmail.com>
2023-06-09 21:58:18 +0100

    Making more changes

---
+++ src/main/java/org/example/MapClass2.java
@@ -0,0 +1,27 @@
+package org.example;
+
+//import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class MapClass2 {
+//    @Test
+    public void mapTest() {
+        Map<String, Integer> StudentsAge= new HashMap<>();
+        StudentsAge.put("Tomi", 25);
+        StudentsAge.put("Yemi", 29);
+        StudentsAge.put("Bismark", 30);
+        StudentsAge.put("Bismark", 40);
+        StudentsAge.put("Ebun", 27);
+        StudentsAge.put("Lateef", 26);
+
+        System.out.println(StudentsAge.get("Bismark"));
+        for(Map.Entry i : StudentsAge.entrySet()){
+            System.out.println(i.getKey() + " " + i.getValue());
+        }
+        System.out.println("hello");
+
+    }
+}
