6da0c7cd92d663bd2b01e05db53460278a59effa
lateef abdulsalam <abdulsalam12000@hotmail.com>
2023-06-09 21:27:54 +0100

    first commit

---
+++ src/main/java/pages/LambdaRegistrationPage.java
@@ -0,0 +1,17 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LambdaMyAccountPage {
+    WebDriver driver;
+    By continueButton = By.linkText("Continue");
+
+    public LambdaMyAccountPage(WebDriver driver){
+        this.driver = driver;
+    }
+    public void  clickOnContinueButton(){
+        driver.findElement(continueButton).click();
+    }
+
+}
